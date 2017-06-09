fe810d7b0f01cab217a7a9f3bbe0ba40eb1b214e
Nguyễn Viết Cường <cuongdotvn@outlook.com>
2017-06-09 17:30:25 +0700

    V12

--- Models/Framework/Category.cs
+++ Models/Framework/Category.cs
@@ -1,7 +1,8 @@
-namespace Models.Framework
+﻿namespace Models.Framework
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -12,18 +13,22 @@ namespace Models.Framework
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "Số kí tự tối đa là 50")]
+        [DisplayName("Tên danh mục")]
+        [Required(ErrorMessage = "Bạn chưa nhập tên danh mục")]
         public string Name { get; set; }
 
         [StringLength(50)]
+        [DisplayName("Tiêu đề SEO")]
         public string Alias { get; set; }
-
+        [DisplayName("Danh mục cha")]
         public int? ParentID { get; set; }
-
+        [DisplayName("Ngày tạo")]
         public DateTime? CreatedDate { get; set; }
-
+        [DisplayName("Thứ tự")]
+        [Range(0, Int32.MaxValue, ErrorMessage = "Bạn phải nhập số")]
         public int? Order { get; set; }
-
+        [DisplayName("Trạng thái")]
         public bool? Status { get; set; }
     }
 }
