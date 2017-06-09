31ffc0e264753a5b85a1183b41aa89fd724f02bc
Nguyễn Viết Cường <cuongdotvn@outlook.com>
2017-06-09 23:38:25 +0700

    v12

--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -1,6 +1,7 @@
 ﻿using Models.Framework;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
@@ -17,7 +18,19 @@ namespace Models
         {
             var list = context.Database.SqlQuery<Category>("Sp_Category_ListAll").ToList();
             return list;
-            
+        }
+        public int Create(string name, string alias, int? parentID, int? order, bool? status)
+        {
+            object[] parameters =
+            {
+                new SqlParameter("@Name", name),
+                new SqlParameter("@Alias", alias),
+                new SqlParameter("@ParentID", parentID),
+                new SqlParameter("@Order", order),
+                new SqlParameter("@Status", status)
+            };
+            int res = context.Database.ExecuteSqlCommand("Sp_Category_Insert @Name,@Alias,@ParentID,@Order,@Status", parameters);
+            return res;
         }
     }
 }
